fcda156e1c727a953052ffb417a6815acdbf24c9
gaowenbin <lotosv2010@163.com>
2021-08-18 15:54:41 +0800

    feat(mobx): mobx 基本使用

--- src/mobx/index.js
+++ src/mobx/index.js
@@ -1,11 +1,16 @@
-// Proxy 使用
-const p = {name: 'proxy'};
-const proxy = new Proxy(p, {
-  get(target, key, receiver) {
-    return Reflect.get(target, key, receiver);
-  },
-  set(target, key, value, receiver) {
-    return Reflect.set(target, key, value, receiver);
-  }
+import {observable, autorun} from 'mobx';
+
+// observable 把普通的数据变成可观察的数据
+// 利用 Object.defineProperty(o, propertyKey, attributes)，不支持监控数组，只能监控已有的属性 
+// 或 new Proxy(target, handler)
+const o = observable({name: 'test'});
+console.dir(o);
+
+// 自动运行，会先运行一次，打印test
+// 属性的值变化后，会执行一次，打印hello
+autorun(() => {
+  console.log(o.name);
 })
-console.dir(proxy.name);
+
+// 属性变化
+o.name = 'hello';
